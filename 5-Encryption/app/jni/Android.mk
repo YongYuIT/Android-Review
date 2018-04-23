58a3130344147a0259d31c7ac59a86dcb20f26a5
yong <yong.yu@hsditech.com>
2018-04-23 14:20:09 +0800

    all abis

--- 5-Encryption/app/jni/Android.mk
+++ 5-Encryption/app/jni/Android.mk
@@ -1,14 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+$(warning "######################$(TARGET_ARCH_ABI)")
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := openssl_cyp
-LOCAL_SRC_FILES := E:\Review\20180326002\Android-Review\5-Encryption\android-arm\lib\libcrypto.so
+LOCAL_SRC_FILES := E:\Review\openssl\android-$(TARGET_ARCH_ABI)\lib\libcrypto.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := com_thinking_encryp
 LOCAL_SRC_FILES := test.cpp
-LOCAL_C_INCLUDES += E:\Review\20180326002\Android-Review\5-Encryption\android-arm\include
+LOCAL_C_INCLUDES += E:\Review\openssl\android-$(TARGET_ARCH_ABI)\include
 LOCAL_LDLIBS +=  -llog -ldl
 LOCAL_SHARED_LIBRARIES := openssl_cyp
 include $(BUILD_SHARED_LIBRARY)
