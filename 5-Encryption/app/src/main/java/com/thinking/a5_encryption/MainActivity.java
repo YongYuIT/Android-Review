a7f6911f06cbcf5c8b9d7573468df21f45e0b967
yong <yong.yu@hsditech.com>
2018-04-26 19:25:11 +0800

    test

--- 5-Encryption/app/src/main/java/com/thinking/a5_encryption/MainActivity.java
+++ 5-Encryption/app/src/main/java/com/thinking/a5_encryption/MainActivity.java
@@ -14,10 +14,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TestTools.do_test();
         txt = "All the presentations will be available on YouTube in about a month. For now, you can take a sneak peek by browsing the slide decks: from Rails 6 to Active Storage, from Migrations to Testing, from Upgrades to API, from Router to Contributing, from GraphQL to Performance, from Collaboration to Communication, from Kafka to PostgreSQL, from Warden to Authorization, from Economy to Art, from Bugs to Security, from Containers to Crypto, from Interviews to History, from Teams to Trust, from Shopify to GitHub, from Engines to Crowdsourcing, from Mortality to Life, there was really something for everyone!";
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
+        txt += txt;
         new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 100; i++) {
+                for (int i = 0; i < 1000; i++) {
                     try {
                         Log.i("yuyong", i + "------------------------" + i);
                         String key = "testtesttesttesttesttesttesttesttest";
@@ -27,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
                         Log.i("yuyong", "--id-->" + id);
                         String de_out = EncrypTools.doDeCryp(key, out);
                         Log.i("yuyong", "--dey-->" + de_out);
-                        Thread.sleep(1000);
                     } catch (Throwable t) {
                         t.printStackTrace();
                     }
